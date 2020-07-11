a94062b86fb2942b096fd95e56d785e3e159e480
brendiTech <brenditech@gmail.com>
2020-07-11 15:42:59 -0300

    Avances home768

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,13 +4,15 @@ import Heroimg from '../images/Heroimg.svg'
 const Hero = () => {
     return(
         <div className="hero">
-            <div className="texts">
-                <h1>Compre de forma inteligente. <br></br>Dirija mais feliz.</h1>
-                <h3>Compare as ofertas de um estoque de mais de 100.000 carros novos e usados.</h3>
-            </div>
-            <div className="buttons">
-                <button className="btn-blue">Comprar carro novo</button>
-                <button className="btn-white">Comprar carro usado</button>
+            <div className="content">
+                <div className="texts">
+                    <h1>Compre de forma inteligente. <br></br>Dirija mais feliz.</h1>
+                    <h3>Compare as ofertas de um estoque de mais de 100.000 carros novos e usados.</h3>
+                </div>
+                <div className="buttons">
+                    <button className="btn-blue">Comprar carro novo</button>
+                    <button className="btn-white">Comprar carro usado</button>
+                </div>
             </div>
             <img src={Heroimg} alt="dibujo" className="ilustracion"/>
         </div>
--- src/components/VerificarPrec.jsx
+++ src/components/VerificarPrec.jsx
@@ -6,9 +6,11 @@ const VerificarPrec = () => {
         <div className="backcolor">
             <div className="verificar">
                 <img src={Verificar} alt="dibujo" className="ilustracion"/>
-                <h2>Obtenha uma oferta de troca que você pode usar imediatamente</h2>
-                <p>Obtenha uma oferta de troca que você pode usar imediatamente Responda a algumas perguntas para ver o valor de mercado do seu carro e obter uma oferta de troca imediata, sem sair da sua casa.</p>
-                <button className="btn-blue">Verificar preç</button>
+                <div className="content">
+                    <h2>Obtenha uma oferta de troca que você pode usar imediatamente</h2>
+                    <p>Obtenha uma oferta de troca que você pode usar imediatamente. <br></br>Responda a algumas perguntas para ver o valor de mercado do seu carro e obter uma oferta de troca imediata, sem sair da sua casa.</p>
+                    <button className="btn-blue">Verificar preç</button>
+                </div>
             </div>
         </div>
     );
