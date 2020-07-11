2df893f8441b1d033d05bf2fa446941b63697e3d
brendiTech <brenditech@gmail.com>
2020-07-11 19:57:52 -0300

    Avances 1024

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,34 +16,36 @@ const Footer = () => {
                         <img src={Instagram} alt="Instagram"/>
                     </div>
                 </div>
-                <div className="links">
-                    <ul className="links">
-                        <li>Carros Novos</li>
-                        <li>Carros Usados</li>
-                        <li>Super ofertas</li>
-                        <li>Cite meu usado</li>
-                        <li>FAQ</li>
-                    </ul>
-                </div>
-                <div className="cliente">
-                    <h3>Atendimento ao Cliente</h3>
-                    <ul>
-                        <li>Seg - Sex 08:00 - 17:00</li>
-                        <li>+55 (11) 5242-0686</li>
-                        <li>ola@karvi.com</li>
-                    </ul>
-                </div>
-                <div className="cons">
-                    <h3>Atendimento às concessionárias</h3>
-                    <ul>
-                        <li>concessionarias@karvi.com</li>
-                    </ul>
+                <div className="content">
+                    <div className="links">
+                        <ul className="links">
+                            <li>Carros Novos</li>
+                            <li>Carros Usados</li>
+                            <li>Super ofertas</li>
+                            <li>Cite meu usado</li>
+                            <li>FAQ</li>
+                        </ul>
+                    </div>
+                    <div className="cliente">
+                        <h3>Atendimento ao Cliente</h3>
+                        <ul>
+                            <li>Seg - Sex 08:00 - 17:00</li>
+                            <li>+55 (11) 5242-0686</li>
+                            <li>ola@karvi.com</li>
+                        </ul>
+                    </div>
+                    <div className="cons">
+                        <h3>Atendimento às concessionárias</h3>
+                        <ul>
+                            <li>concessionarias@karvi.com</li>
+                        </ul>
+                    </div>
                 </div>
 
             </div>
             <div className="divbottom">
                     <h3>Termos e Condições-Política de Privacidade</h3>
-                </div>
+            </div>
         </div>
 
     );
